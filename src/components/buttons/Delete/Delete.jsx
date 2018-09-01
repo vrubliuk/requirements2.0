dcb995854dc2329e9fa69b031659ed4dd6b933b1
Valentyn <vrubliuk@gmail.com>
2018-09-01 15:16:14 +0300

    Add a deletion confirmation dialog

---
+++ src/components/buttons/Delete/Delete.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Delete.css";
+import colors from "../../../assets/colors";
+import { withRouter } from "react-router-dom";
+
+const Delete = ({ location, handleClick }) => {
+  const color = colors[location.pathname].dark;
+  return (
+    <div className="Delete" style={{ borderColor: color, color }} onClick={handleClick} >
+      <i className="fa fa-trash" aria-hidden="true" />
+    </div>
+  );
+};
+
+export default withRouter(Delete);
--- src/components/popups/EditRequirementsRow/EditRequirementsRow.jsx
+++ src/components/popups/EditRequirementsRow/EditRequirementsRow.jsx
@@ -4,13 +4,15 @@ import colors from "../../../assets/colors";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import * as actionCreators from "../../../store/actions/actionCreators";
+import Delete from "../../buttons/Delete/Delete.jsx";
 
 class EditRequirementsRow extends Component {
   state = {
     сustomer: "",
     documentation: "",
     releaseSheetLink: "",
-    releaseSheetAE: ""
+    releaseSheetAE: "",
+    showDeletionConfirmation: false
   };
 
   updateInputs = () => {
@@ -37,16 +39,41 @@ class EditRequirementsRow extends Component {
   };
 
   handleDelete = () => {
-    this.props.initDeleteRow("requirements", this.props.data.id)
-  }
+    this.props.initDeleteRow("requirements", this.props.data.id);
+  };
 
   componentDidMount() {
-   
     this.updateInputs();
   }
 
   render() {
     const color = colors[this.props.location.pathname].dark;
+    const footers = {
+      1: (
+        <div className="Modal__footer">
+          <button style={{ background: color }} type="submit">
+            Save
+          </button>
+          <Delete handleClick={()=> this.setState({showDeletionConfirmation: true})} />
+        </div>
+      ),
+      2: (
+        <div className="Modal__footer">
+          <div className="Modal__buttonGroup">
+            <button className="Modal__button-secondary" style={{ borderColor: color, color }} type="button" onClick={this.handleDelete}>
+              Delete
+            </button>
+            <button style={{ background: color}} type="button" onClick={()=> this.setState({showDeletionConfirmation: false})}>
+              Keep
+            </button>
+          </div>
+          <div className="Modal__error" style={{ color }}>
+            Are you sure you want to delete this row?
+          </div>
+        </div>
+      )
+    };
+
     return (
       <div className="EditRequirementsRow">
         <div className="Modal__title">Edit requirement</div>
@@ -59,14 +86,7 @@ class EditRequirementsRow extends Component {
           <input className="Modal__input" type="text" value={this.state.releaseSheetLink} onChange={e => this.handleInput(e, "releaseSheetLink")} />
           <div className="Modal__label">Responsible AE for release sheet</div>
           <input className="Modal__input" type="text" value={this.state.releaseSheetAE} onChange={e => this.handleInput(e, "releaseSheetAE")} />
-          <div className="Modal__footer">
-            <button style={{ background: color }} type="submit">
-              Save
-            </button>
-            <button className="Modal__button-secondary" style={{ borderColor: color, color }} type="button" onClick={this.handleDelete}>
-              Delete
-            </button>
-          </div>
+          {!this.state.showDeletionConfirmation ? footers[1] : footers[2]}
         </form>
       </div>
     );
