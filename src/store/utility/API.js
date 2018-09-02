55b5503ee4f91c9e7110aeb82f7b81266eb56385
Valentyn <vrubliuk@gmail.com>
2018-09-02 22:44:33 +0300

    Always show the vertical scroll bar

--- src/store/utility/API.js
+++ src/store/utility/API.js
@@ -8,57 +8,9 @@ export const postRefreshToken = () => {
   return axios.post("https://securetoken.googleapis.com/v1/token?key=AIzaSyAEyDsrBHsnHoEItk9PgshoujhrTk0ZlAA", { grant_type: "refresh_token", refresh_token: localStorage.refreshTokenRequirements });
 };
 
-// export const getRequirements = () => {
-//   return axios.get("https://requirements-c506c.firebaseio.com/requirements.json")
-// }
-
-// export const getOffices = () => {
-//   return axios.get("https://requirements-c506c.firebaseio.com/offices.json")
-// }
-
-// export const getRailLoads1 = () => {
-//   return axios.get("https://requirements-c506c.firebaseio.com/railLoads1.json")
-// }
-
-// export const getRailLoads2 = () => {
-//   return axios.get("https://requirements-c506c.firebaseio.com/railLoads2.json")
-// }
-
 export const getData = (table) => {
   return axios.get(`https://requirements-c506c.firebaseio.com/${table}.json`)
 }
-
-// export const postRequirement = (token, сustomer, documentation, releaseSheetLink, releaseSheetAE) => {
-//   return axios.post(`https://requirements-c506c.firebaseio.com/requirements.json?auth=${token}`, {
-//     сustomer,
-//     documentation,
-//     releaseSheetLink,
-//     releaseSheetAE
-//   });
-// };
-
-// export const postOffices = (token, office, abbreviation, leader, GSM) => {
-//   return axios.post(`https://requirements-c506c.firebaseio.com/offices.json?auth=${token}`, {
-//     office,
-//     abbreviation,
-//     leader,
-//     GSM
-//   });
-// };
-
-// export const postRailLoads1 = (token, SCAC, carrier) => {
-//   return axios.post(`https://requirements-c506c.firebaseio.com/railLoads1.json?auth=${token}`, {
-//     SCAC,
-//     carrier
-//   });
-// };
-
-// export const postRailLoads2 = (token, equipmentType, abbreviation) => {
-//   return axios.post(`https://requirements-c506c.firebaseio.com/railLoads2.json?auth=${token}`, {
-//     equipmentType,
-//     abbreviation
-//   });
-// };
 
 export const postRow = (token, table, row) => {
   return axios.post(`https://requirements-c506c.firebaseio.com/${table}.json?auth=${token}`, row);
