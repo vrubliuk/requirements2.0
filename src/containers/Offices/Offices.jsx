69b6ad269d295acb0ba09a3daee0574597b3b9c8
Valentyn <vrubliuk@gmail.com>
2018-09-17 20:00:58 +0300

    Sort the offices table directly in the container

--- src/containers/Offices/Offices.jsx
+++ src/containers/Offices/Offices.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Offices.css";
-import SortedOfficesTable from "../../components/SortedOfficesTable/SortedOfficesTable.jsx"
+import Table from "../../components/Table/Table.jsx";
 import { connect } from "react-redux";
 
 const Offices = ({ offices }) => {
@@ -32,11 +32,60 @@ const Offices = ({ offices }) => {
     }
   ];
 
-  return (
-    <div className="Offices">
-      <div className="Offices__inner">{Object.keys(offices).length ? <SortedOfficesTable table="offices" data={offices} columns={columns} /> : null}</div>
-    </div>
-  );
+  const transformToArray = data => {
+    return Object.keys(data).map(key => {
+      let values = {};
+      Object.keys(data[key]).forEach(value => {
+        values[value] = data[key][value];
+      });
+      return { key, ...values };
+    });
+  };
+
+  const sortAlphabetically = data => {
+    return data.sort((a, b) => {
+      const x = a.office.toLowerCase();
+      const y = b.office.toLowerCase();
+      if (x > y) {
+        return 1;
+      }
+      if (x < y) {
+        return -1;
+      }
+      return 0;
+    });
+  };
+
+  const transformToObject = data => {
+    let obj = {};
+    data.forEach(item => {
+      obj[item.key] = {};
+      Object.keys(item).forEach(value => {
+        if (value !== "key") obj[item.key][value] = item[value];
+      });
+    });
+    return obj;
+  };
+
+  let sortedOffices, content;
+
+  if (Object.keys(offices).length) {
+    const transformedToArray = transformToArray(offices);
+    const sorted = sortAlphabetically(transformedToArray);
+    const transformedToObject = transformToObject(sorted);
+    sortedOffices = transformedToObject;
+    content = (
+      <div className="Offices">
+        <div className="Offices__inner">
+          <Table table="offices" data={sortedOffices} columns={columns} />
+        </div>
+      </div>
+    );
+  } else {
+    content = null;
+  }
+
+  return content;
 };
 
 const mapStateToProps = state => {
